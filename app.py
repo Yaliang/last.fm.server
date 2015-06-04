1bd71c927f5538a80accc80291dd793e43683922
Yaliang <yaliangwang2015@u.northwestern.edu>
2015-06-03 22:18:56 -0500

    test data using pickle

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import os
 from flask import Flask
+import pickle
+from Artist import *
+from User import *
+from KNN import *
+
+def save(dObj, sFilename):
+    """Given an object and a file name, write the object to the file using pickle."""
+
+    f = open(sFilename, "w")
+    p = pickle.Pickler(f)
+    p.dump(dObj)
+    f.close()
+
+def load(sFilename):
+    """Given a file name, load and return the object stored in the file."""
+
+    f = open(sFilename, "r")
+    u = pickle.Unpickler(f)
+    dObj = u.load()
+    f.close()
+    return dObj
 
 # initialization
+# load data
+pathToData = os.path.join(app.root_path, 'static','data','hetrec2011-lastfm-2k-built')
+UserManager = load(os.path.join(pathToData,'user-manager.pkr'))
+ArtistManager = load(os.path.join(pathToData, 'artist-manager.pkr'))
+TrainUserManager = load(os.path.join(pathToData, 'train-user-manager.pkr'))
+# build app
 app = Flask(__name__)
 app.config.update(
     DEBUG = True,
@@ -10,11 +37,7 @@ app.config.update(
 #controllers
 @app.route("/")
 def index():
-    pathToData = os.path.join(app.root_path, 'static','data')
-    f = open(os.path.join(pathToData, 'testdata.data'),'r')
-    data = f.readline()
-    f.close()
-    return data
+    return 'user number:'+str(len(UserManager))+'; artist number:'+str(len(ArtistManager))
 
 # launch
 if __name__ == "__main__":
